1952d4100043c80960d36ce1a86e7c1ec629607e
Ran Shaked <ran.shaked@runi.ac.il>
2024-12-11 23:03:10 +0200

    hw5

--- Scrabble.java
+++ Scrabble.java
@@ -118,7 +118,8 @@ public class Scrabble {
 				System.out.println("");
 			}else{
 				if(!MyString.subsetOf(input,hand)){
-					System.out.println("No such word in the dictionary. Try again.");
+					System.out.println("Invalid word. Try again.");
+					System.out.println("");
 				}else{
 					int wordScore = wordScore(input);
 					score += wordScore(input);
